3e816e2248ef4057ca0953b812e3bcb74851f27c
devjiwonchoi <devjiwonchoi@gmail.com>
2025-02-18 22:00:03 +0900

    use ref

--- packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/errors/dev-tools-indicator/dev-tools-indicator.stories.tsx
+++ packages/next/src/client/components/react-dev-overlay/_experimental/internal/components/errors/dev-tools-indicator/dev-tools-indicator.stories.tsx
@@ -62,7 +62,7 @@ const state: OverlayState = {
   debugInfo: { devtoolsFrontendUrl: undefined },
 }
 
-export const NoErrors: Story = {
+export const StaticRoute: Story = {
   args: {
     errorCount: 0,
     state,
